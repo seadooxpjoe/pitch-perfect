754ec13746a4cfc6728ac19b7a95db44a1c0db91
seadooxpjoe <seadooxpjoe@v1online.net>
2015-04-06 21:21:25 -0400

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by user on 4/6/15.
+//  Copyright (c) 2015 V1 Online. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
